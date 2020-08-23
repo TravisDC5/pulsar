cc821776c71a06f2935b01bceceda108d86e62b7
TravisDC5 <61341968+TravisDC5@users.noreply.github.com>
2020-08-22 22:16:24 -0700

    added randomforest and charts html pages

--- dataConvert/dataConversion.py
+++ dataConvert/dataConversion.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
-data_df = pd.read_csv('../modelmaker/data.csv')
+data_df = pd.read_csv('../modelmaker/HTRU_2.csv')
+
+data_df.columns = ['1','2','3','4','5','6','7','8','9']
 
 #print(data_df)
 
