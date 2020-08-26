af02f2227c856aee41fd0d502c9223fa0ca1ba53
TravisDC5 <61341968+TravisDC5@users.noreply.github.com>
2020-08-26 11:56:01 -0700

    populated dropdown with RF id's

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
-import os
+import pandas as pd
 from flask import Flask, render_template, json, current_app, app, request, \
     url_for, redirect
-
-# static/data/test_data.json
-# dataFile = os.path.join(app.static_folder, 'data', 'test_data.json')
-
-# with open(dataFile) as test_file:
-#     data = json.load(test_file)
 
 app = Flask(__name__)
 
@@ -32,13 +26,30 @@ def randomforest():
 
 @app.route("/ocr")
 def ocr():
-     return render_template('ocr.html',
-        data=[{'name':'red'}, {'name':'green'}, {'name':'blue'}])
+    
+    
+    data_df = pd.read_csv('modelmaker/HTRU_2.csv')
+    data_df.columns = ['1','2','3','4','5','6','7','8','9']
+    data_df['10'] = data_df.index
+    select_df = data_df.iloc[4084:4104, 9]
+
+    id_data = []
+    d1 = {}
+    for o in select_df:
+        d1 = dict()
+        d1['id'] = o
+        id_data.append(d1)
+    
+    
+    
+    return render_template('ocr.html', data=id_data)
+    
 
 @app.route("/model" , methods=['GET', 'POST'])
 def model():
     select = request.form.get('comp_select')
     return(str(select)) # just to see what select is
+    
 
 if __name__ == "__main__":
     app.run()
