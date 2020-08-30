195bdc0517c0ba52fe247e133e0c658fe79137a1
TravisDC5 <61341968+TravisDC5@users.noreply.github.com>
2020-08-30 13:21:38 -0700

    added human

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import pickle
 from flask import Flask, render_template, json, current_app, app, request, url_for, redirect
 from flask_pymongo import PyMongo
@@ -38,6 +39,7 @@ def randomforest():
 def ocr():
 
     model = mongo.db.model.find_one()
+    
 
     data_df = pd.read_csv('modelmaker/HTRU_2.csv')
     data_df.columns = ['1','2','3','4','5','6','7','8','9']
@@ -90,13 +92,101 @@ def retrieve():
         model_output = "Not a Pulsar"
     else:
         model_output = "Is a Pulsar"
-    print(model_output)
+    # print(model_output)
 
 
     model = mongo.db.model
     model_data = {'selection' : select, 'class_acc': cla, 'class_pre': model_output}
     model.replace_one({},model_data, upsert=True)
 
+
+    return redirect("/rfmodel", code=302)
+
+@app.route("/retrieve2" , methods=['GET', 'POST'])
+def retrieve2():
+    model = mongo.db.model.find_one()
+    
+    fname = request.form.get('fname')
+    age = request.form.get('age')
+    sibling = request.form.get('sibling')
+    pets = request.form.get('pets')
+    origin = request.form.get('origin')
+    city = request.form.get('city')
+    hours = request.form.get('hours')
+    pnumber = request.form.get('pnumber')
+
+    # Get Ascii values for name
+    nameValue = fname
+    ascValue = []
+
+    for name in nameValue:
+        for ch in name:
+            ascValue.append(ord(ch))
+
+    charCount = len(ascValue)
+    valSum = sum(ascValue)
+
+    fnameValue = round(valSum/charCount,2)
+    
+    # Get Ascii values for origin
+    originValue = origin
+    ascValue2 = []
+
+    for name in originValue:
+        for ch in name:
+            ascValue2.append(ord(ch))
+
+    charCount2 = len(ascValue2)
+    valSum2 = sum(ascValue2)
+
+    originValueAsc = round(valSum2/charCount2,2)
+
+    # Get Ascii values for currenct city
+    cityValue = city
+    ascValue3 = []
+
+    for name in cityValue:
+        for ch in name:
+            ascValue3.append(ord(ch))
+
+    charCount3 = len(ascValue3)
+    valSum3 = sum(ascValue3)
+
+    cityValueAsc = round(valSum3/charCount3,2)
+    
+    # Run Data through Model
+    dataArray = np.array([fnameValue, age, sibling, pets, originValueAsc, cityValueAsc, hours, pnumber] , dtype=float)
+
+    pickle_file = "modelmaker/RFC_model_v1_12_3_175.h5"
+
+    with open(pickle_file, 'rb') as file:
+        Pickled_RF_Model = pickle.load(file)
+    
+    X = dataArray
+
+    y_pred = Pickled_RF_Model.predict(X)
+
+    if y_pred[0] == 0:
+        model_output2 = "Not a Pulsar"
+    else:
+        model_output2 = "a Pulsar"
+
+    
+    model = mongo.db.model
+    model_data = {'firstName' : fname,
+                  'personAge': age, 
+                  'nameValueAVG': fnameValue,
+                  'sibCount': sibling,
+                  'numPets': pets,
+                  'origin': origin,
+                  'originValueAsc': originValueAsc,
+                  'currCity': city,
+                  'cityValueAsc': cityValueAsc,
+                  'labHours': hours,
+                  'favNumber': pnumber,
+                  'humanPulsar': model_output2
+                  }
+    model.replace_one({},model_data, upsert=True)
 
     return redirect("/rfmodel", code=302)
 
